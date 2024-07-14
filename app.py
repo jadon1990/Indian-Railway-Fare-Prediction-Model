efbc0212f4f7a6c183503e954f1398c76dff9cc0
Ankit Jadon <mail2ankitjadon@gmail.com>
2024-07-14 23:30:37 +0530

    final

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+pipe = pickle.load(open('pipe.pkl', 'rb'))
+data = pd.read_pickle('rouute.pkl')
+stationDetails = pd.read_pickle('station.pkl')
+
+data = pd.read_pickle('rouute.pkl')
+stationFrom = data['stationCode'].unique()
+
+st.title('Train ticket Fare Prediction')
+
+col1, col2 =  st.columns([1,2])
+with col1:
+    station_from = st.selectbox('Station From', sorted(stationFrom))
+
+
+stationFrom = station_from
+
+stationTo = data[data['stationCode'] == stationFrom]['stationTo'].unique()
+
+with col2:
+    station_To = st.selectbox('Station To', sorted(stationTo))
+
+
+if st.button('Predict Fare'):
+
+    input_data = data.loc[(data['stationCode'] == station_from) & (data['stationTo'] == station_To),['distance', 'journeyTime', 'trainStatus']]
+
+    result = pipe.predict(input_data)
+    st.header("Predicted Score is : " + str(int(result[0])))
+
