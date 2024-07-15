72cbfb146758580ce64ead1a07bc5ea5be546101
Ankit Jadon <47268931+jadon1990@users.noreply.github.com>
2024-07-15 11:03:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
-import sklearn
+#ML Libraries
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import r2_score, mean_absolute_error
+from xgboost import XGBRegressor
+
 import pickle
 import pandas as pd
 import numpy as np
